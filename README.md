24bc8c58ef3581e63eb21ed4223bddf655b9bc70
Rosuelo996 <rosuelo996.sr@gmail.com>
2026-01-03 00:18:26 +0000

    final project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Car-Subscription
+# Car-Subscription
---
+++ findyourcar.js
@@ -0,0 +1,232 @@
+const cardListEl = document.querySelector(".card__list");
+const searchInputEl = document.querySelector(".header__input");
+const emptyStateEl = document.querySelector(".search__empty");
+const searchTitleEl = document.querySelector(".search__title");
+const searchProgress = document.getElementById("searchProgress");
+const searchLoading = document.getElementById("searchLoading");
+
+const minRange = document.getElementById("minRange");
+const maxRange = document.getElementById("maxRange");
+const priceText = document.getElementById("priceText");
+
+const allowedMakes = [
+  "toyota",
+  "audi",
+  "bmw",
+  "mercedes",
+  "ford",
+  "tesla",
+  "volkswagen",
+  "honda",
+  "hyundai",
+];
+
+const MAX_RESULTS = 9;
+
+// STATE
+
+let carsData = [];
+
+
+// API
+
+async function getModelsForMake(make) {
+  const url = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/${make}?format=json`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.Results;
+}
+
+// RANDOM DATA
+
+function randomPrice() {
+  const min = 10000;
+  const max = 100000;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomMileage() {
+  const min = 0;
+  const max = 12000;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomBodyType() {
+  const types = ["Sedan", "SUV", "Coupe", "Hybrid", "Wagon", "Convertible"];
+  return types[Math.floor(Math.random() * types.length)];
+}
+
+// IMAGES
+function getCarImage(make, modelName) {
+
+  const formattedModel =
+    modelName.replace(/\s+/g, "-");
+
+  return `./assets/cars/${make}/${formattedModel}.jpg`;
+}
+
+// MAIN
+
+async function loadCars(make) {
+  const apiModels = await getModelsForMake(make);
+  const limitedModels = apiModels.slice(0, MAX_RESULTS);
+
+  carsData = limitedModels.map((apiModel) => {
+    return {
+      make: apiModel.Make_Name,
+      model: apiModel.Model_Name,
+      price: randomPrice(),
+      mileage: randomMileage(),
+      bodyType: randomBodyType(),
+      img: getCarImage(make, apiModel.Model_Name),
+    };
+  });
+
+  filterSortAndRender();
+}
+
+// FILTER + SORT + OUTPUT
+
+function filterSortAndRender() {
+  const minPrice = Number(minRange.value);
+  const maxPrice = Number(maxRange.value);
+
+  // filter
+  const filteredCars = carsData
+  
+  .filter((car) => {
+    return car.price >= minPrice && car.price <= maxPrice;
+  })
+
+  // sort
+  .sort((a, b) => b.price - a.price);
+
+  // render + empty state
+  if (filteredCars.length > 0) {
+    emptyStateEl.classList.add("hidden");
+    cardListEl.innerHTML = filteredCars
+      .map((car) =>
+        modelHTML(car))
+      .join("");
+  } else {
+    cardListEl.innerHTML = "";
+    emptyStateEl.classList.remove("hidden");
+  }
+}
+
+// CAR CARD
+
+function modelHTML(car) {
+  return `
+    <div class="card">
+      <div class="card__img--wrapper">
+        <img class="card__img" src="${car.img}"
+          alt="${car.make} ${car.model}"
+          onerror="this.src='./assets/car-sample.jpg'" />
+        <div class="card__overlay">
+          <span>More info</span>
+        </div>
+      </div>
+
+      <div class="card__description">
+        <h3 class="card__title purple">${car.make} ${car.model}</h3>
+
+        <div class="card__spec">
+          <i class="fa-solid fa-car-side card__icon"></i>
+          <span>${car.bodyType}</span>
+        </div>
+
+        <div class="card__spec">
+          <i class="fa-solid fa-gears card__icon"></i>
+          <span>Automatic</span>
+        </div>
+
+        <div class="card__spec">
+          <i class="fa-solid fa-gauge card__icon"></i>
+          <span>${car.mileage.toLocaleString()} km</span>
+        </div>
+
+        <div class="car-card__price purple">£${car.price.toLocaleString()}</div>
+      </div>
+    </div>
+  `;
+}
+
+// SEARCH BUTTON
+
+function searchCars() {
+  const rawInput = searchInputEl.value;
+  const searchTerm = rawInput.trim().toLowerCase();
+
+  // search results
+  if (searchTerm !== "") {
+    searchTitleEl.innerHTML = `Search results for "<span class="purple">${rawInput}</span>"`;
+  } else {
+    searchTitleEl.innerHTML = "Search results:";
+  }
+
+  // show loading
+  searchProgress.classList.remove("hidden");
+  searchLoading.classList.remove("hidden");
+  emptyStateEl.classList.add("hidden");
+  cardListEl.innerHTML = "";
+
+  // valid search / invalid search
+  setTimeout(() => {
+    if (searchTerm !== "" && allowedMakes.includes(searchTerm)) {
+      loadCars(searchTerm);
+    } else {
+      emptyStateEl.classList.remove("hidden");
+    }
+
+  // hide loading
+  searchProgress.classList.add("hidden");
+  searchLoading.classList.add("hidden");
+  }, 1000);
+}
+
+// RESET
+
+function resetSearch() {
+  searchInputEl.value = "";
+  searchTitleEl.innerHTML = "Search results:";
+  emptyStateEl.classList.add("hidden");
+
+  // reset sliders
+  minRange.value = 0;
+  maxRange.value = 100000;
+  updatePrice();
+
+  loadCars("mercedes");
+}
+
+// SLIDER TEXT + FILTER TRIGGER
+
+function updatePrice() {
+  let min = Number(minRange.value);
+  let max = Number(maxRange.value);
+
+  if (min > max) {
+    minRange.value = max;
+    min = Number(minRange.value);
+  }
+
+  priceText.textContent = `£${min.toLocaleString()} to £${max.toLocaleString()}`;
+
+  filterSortAndRender();
+}
+
+// INITIAL LOAD
+
+updatePrice();
+loadCars("mercedes");
+
+// MENU
+
+function openMenu() {
+  document.body.classList.add("menu--open");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
